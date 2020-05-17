2b892f16ae81cf8de9da97a87fc81ce1c0d96357
papi <kevinaudred@gmail.com>
2020-05-17 22:39:37 +0100

    update codebase

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+ app.use(express.static(__dirname +'/dist/'));
+ app.get('/*',(req,res,next)=>{
+ 
+ res.sendFile(path.join(__dirname, '/dist/index.html'));
+ });
+ 
+ app.listen(process.env.PORT || 8000, () =>{
+           console.log('Server ecoute sur le port %s' , process.env.PORT || 8080)
+ })
